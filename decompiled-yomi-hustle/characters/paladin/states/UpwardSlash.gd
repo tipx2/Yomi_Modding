221e3388108f1be3b5686d4b9f516a4f86e054f8
tipx2 <57505742+tipx2@users.noreply.github.com>
2022-12-06 14:15:05 +0000

    updated sprites and other hitbox stuff

--- decompiled-yomi-hustle/characters/paladin/states/UpwardSlash.gd
+++ decompiled-yomi-hustle/characters/paladin/states/UpwardSlash.gd
@@ -4,7 +4,7 @@ export  var GROUNDED_LIFT = "-15.0"
 export  var AERIAL_LIFT = "-7.0"
 
 
-func _frame_7():
+func _frame_8():
 	var vel = host.get_vel()
 	host.set_vel(vel.x, "0")
 	host.apply_force_relative("0", GROUNDED_LIFT if host.is_grounded() else AERIAL_LIFT)
